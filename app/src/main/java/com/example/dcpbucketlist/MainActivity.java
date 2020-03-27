0131cccab9b5a3e8b1d65abe2afd792af9fddbc1
Mitchell Ferguson <mitchell.ferguson1998@gmail.com>
2020-03-27 15:09:30 -0400

    Test commit

--- app/src/main/java/com/example/dcpbucketlist/MainActivity.java
+++ app/src/main/java/com/example/dcpbucketlist/MainActivity.java
@@ -21,7 +21,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         copyAssets();
-        // Hello
     }
 
     private void startEvent(String park) {
